21cb0301c240494049f6c2ceeaa8a4bbb6c6bec8
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-25 13:16:51 +0700

    add GetOwners and GetOwner actions

---
+++ aspnetcore-crud/Controllers/OwnerController.cs
@@ -0,0 +1,36 @@
+﻿using aspnetcore_crud.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore_crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private readonly IOwnerRepository _ownerRepository;
+
+        public OwnerController(IOwnerRepository ownerRepository)
+        {
+            _ownerRepository = ownerRepository;
+        }
+
+        [HttpGet(Name = "GetOwners")]
+        public async Task<IActionResult> GetOwners()
+        {
+            var owners = await _ownerRepository.GetOwners();
+            return Ok(owners);
+        }
+
+        [HttpGet("{id}", Name = "GetOwner")]
+        public async Task<IActionResult> GetOwner(int id)
+        {
+            var owner = await _ownerRepository.GetOwner(id);
+            if(owner == null)
+            {
+                return NotFound();
+            }
+            return Ok(owner);
+        }
+    }
+}
