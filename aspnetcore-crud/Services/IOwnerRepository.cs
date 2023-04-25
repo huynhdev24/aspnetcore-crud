a27c465e563fb020c27c530bf1f4a3e2fc5b0506
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-25 12:19:00 +0700

    define Owner Interface Repository

---
+++ aspnetcore-crud/Services/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using aspnetcore_crud.Models;
+
+namespace aspnetcore_crud.Services
+{
+    public interface IOwnerRepository
+    {
+        Task<IEnumerable<Owner>> GetOwners();
+        Task<Owner> GetOwner(int ownerId);
+        Task<Owner> GetOwnerWithDetails(int ownerId);
+        void CreateOwner(Owner owner);
+        void UpdateOwner(Owner dbowner, Owner owner);
+    }
+}
