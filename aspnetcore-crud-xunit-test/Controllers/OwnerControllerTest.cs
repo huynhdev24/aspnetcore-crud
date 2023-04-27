1cc50c0db46b9f1a26591ab8d16896cd8d394c26
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-27 14:12:50 +0700

    Initital project Unit Test

---
+++ aspnetcore-crud-xunit-test/Controllers/OwnerControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnetcore_crud_xunit_test.Controllers
+{
+    internal class OwnerControllerTest
+    {
+    }
+}
